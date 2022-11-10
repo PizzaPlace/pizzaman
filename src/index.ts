605f6891c6d251e6b6c8847e59034fa97ff16e30
Fad F <fmzfads@gmail.com>
2022-11-10 23:57:14 +0500

    TODO: complete index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import yargs from "yargs/yargs"
 import { hideBin } from "yargs/helpers"
+import FileManager from "./modules/FileManager/FileManager"
+import path from "path"
 
 const argv = yargs(hideBin(process.argv)).argv
 
-console.log(argv)
+// SOON (tm)
