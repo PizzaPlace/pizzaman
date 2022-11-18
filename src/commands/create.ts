26b12d2374860455ac25b4b04d896155c01b5458
Fad F <fmzfads@gmail.com>
2022-11-18 16:50:10 +0500

    added a new cli command: create <type> <name>

---
+++ src/commands/create.ts
@@ -0,0 +1,33 @@
+import chalk from 'chalk'
+import { Argument, Command } from 'commander'
+import { createSpinner } from 'nanospinner'
+import Cli from '../modules/Cli'
+import FileManager from '../modules/FileManager'
+import { ComponentType } from '../typings/ComponentType'
+
+const command = new Command()
+    .command('create')
+    .description('create something idfk')
+    .addArgument(
+        new Argument(
+            '<type>',
+            'The type of component that you want to create'
+        ).choices(['command', 'module', 'event'])
+    )
+    .argument('<name>', 'Name of the component')
+    .action(async (type: ComponentType, name: string, opts: object) => {
+        Cli.load({
+            callback: async () => {
+                return await new FileManager({
+                    command: '__tests__/commands',
+                    event: '__tests__/events',
+                    module: '__tests/modules',
+                }).create(type, name)
+            },
+            beforeText: `Creating ${type}`,
+            component: type,
+            afterText: `${type} '${name}' has been created`,
+        })
+    })
+
+export default command
